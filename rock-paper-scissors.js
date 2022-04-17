f83ef582966414b5d8ffbd2b059e758cc8183b55
Marion <marion.peralta88@gmail.com>
2022-04-17 10:59:58 -0700

    create if else to return player choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,12 +4,17 @@
 // 3. Player will input their choice
 
 //Pseudocode
-// 1. create computerPlay function to have computer spit out a choice
-//  a. declare function (no parameters)
-//  b. assign number to variable (e.g. let rock = 0)
-//  c. use a random number generator to produce a number from 0-2 and store in computerChoice
-//  d. use if-else to return the a string associated with that number (e.g. if rand number is 1, return paper)
-// 2. create rpsRound function
+// 1. DONE create computerPlay function to have computer spit out a choice
+//  a. DONE declare function (no parameters)
+//  b. DONE assign number to variable (e.g. let rock = 0)
+//  c. DONE use a random number generator to produce a number from 0-2 and store in computerChoice
+//  d. DONE use if-else to return the a string associated with that number (e.g. if rand number is 1, return paper)
+// 2. Create playerChoice function
+//  - DONE prompt player for choice
+//  - DONE loop if player does not enter the right values
+//  - capitalize playerChoice
+//  - return playerChoice
+// 3. create rpsRound function
 //  a. accepts two parameters which are player choice computer choice
 //  b. prompt player for their choice (e.g. rock, paper or scissors) or if they want to exit
 //  - if exit, then close program, else move on to next step
@@ -44,4 +49,32 @@ function computerPlay() {
 
 }
 
-let computerChoice = computerPlay();
+function playerChoice() {
+    let choice = prompt("Enter ROCK, PAPER, OR SCISSORS. Enter END to close");
+    upperCaseChoice = choice.toUpperCase();
+
+    if (upperCaseChoice == "END") {
+        alert("Thanks for playing!")
+        return "END";
+    }
+    if (upperCaseChoice == "ROCK") {
+        return "ROCK";
+    }
+
+    else if (upperCaseChoice == "PAPER") {
+        return "PAPER";
+    }
+
+    else if (upperCaseChoice == "SCISSORS") {
+        return "SCISSORS";
+    }
+
+    else {
+        alert('Invalid choice')
+        playerChoice();
+    }
+
+
+}
+
+playerChoice();
