816a2a34259f1ba94502f698aecd6e07680b43fb
Marion <marion.peralta88@gmail.com>
2022-04-17 10:29:03 -0700

    implement if else statement to return choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -11,7 +11,6 @@
 //  d. use if-else to return the a string associated with that number (e.g. if rand number is 1, return paper)
 // 2. create rpsRound function
 //  a. accepts two parameters which are player choice computer choice
-//  b. convert computerChoice to uppercase
 //  b. prompt player for their choice (e.g. rock, paper or scissors) or if they want to exit
 //  - if exit, then close program, else move on to next step
 //  c. convert player choice to uppercase 
@@ -25,10 +24,24 @@ function computerPlay() {
     const scissors = 2;
 
     let computerChoice = Math.floor(Math.random() * 3); //generates a random number from 0-2 which represents rock, paper, and scissors
-    
 
+    if (computerChoice == 0) {
+        return "ROCK";
+    }
+
+    else if (computerChoice == 1) {
+        return "PAPER";
+    }
+
+    else if (computerChoice == 2) {
+        return "SCISSORS";
+    }
+
+    else {
+        alert("computerPlay messed up somehow, returning SCISSORS")
+    }
 
 
 }
 
-computerPlay();
+let computerChoice = computerPlay();
